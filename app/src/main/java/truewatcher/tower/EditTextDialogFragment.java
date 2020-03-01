9463ed639317c8ba1dff44c33cec178d8354f385
TrueWatcher <rosc71@hotmail.com>
2020-03-01 21:23:31 +0300

    2.5.1 improvements

--- app/src/main/java/truewatcher/tower/EditTextDialogFragment.java
+++ app/src/main/java/truewatcher/tower/EditTextDialogFragment.java
@@ -53,7 +53,7 @@ public class EditTextDialogFragment extends DialogFragment {
     mId=Integer.valueOf(this.getArguments().getString("actionId"));
     mStringId=Integer.valueOf(this.getArguments().getString("actionStringId"));
     mText=this.getArguments().getString("text");
-    if (mId == 0 || mStringId == 0 || mText == null || mText.isEmpty()) throw new U.RunException("EditTextDialogFragment:Missing arguments");
+    if (mId == 0 || mStringId == 0 || mText == null) throw new U.RunException("EditTextDialogFragment:Missing arguments");
 
     AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(getActivity());
     builder.setMessage(mStringId);
@@ -63,6 +63,7 @@ public class EditTextDialogFragment extends DialogFragment {
                           LinearLayout.LayoutParams.MATCH_PARENT,
                           LinearLayout.LayoutParams.MATCH_PARENT);
     mInput.setLayoutParams(lp);
+    if (mText.isEmpty()) mInput.setHint("<enter text>");
     mInput.setText(mText);
     builder.setView(mInput);
     
